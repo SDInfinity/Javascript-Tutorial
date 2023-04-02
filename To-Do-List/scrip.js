443bb2de0b1eb8b9a50e4b2ff17fb77575c3bf55
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-04-02 22:57:40 +0530

    add local Storage folder

---
+++ To-Do-List/scrip.js
@@ -0,0 +1,25 @@
+let key = document.querySelector(".key");
+
+let value = document.querySelector(".value");
+
+
+let div1 = document.querySelector(".display-1");
+let div2 = document.querySelector(".display-2");
+
+
+
+let btn = document.querySelector(".btn");
+
+console.log(btn);
+
+
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    localStorage.setItem(`${key.value}`,`${value.value}`);
+    div1.innerHTML+=`${key.value}`;
+    div2.innerHTML+=`${value.value}`;
+})
+
+div1.innerHTML+="";
+div2.innerHTML+="";
