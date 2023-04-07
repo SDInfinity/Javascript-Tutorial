5fd8b63b3e85a96146d6bcec73275d43c63f3326
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-04-07 22:31:30 +0530

    Modify To-Do-List App

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,6 +1,6 @@
 let key = document.querySelector(".key");
 
-let value = document.querySelector(".value");
+let description = document.querySelector(".value");
 
 
 let div1 = document.querySelector(".display-1");
@@ -8,18 +8,53 @@ let div2 = document.querySelector(".display-2");
 
 
 
-let btn = document.querySelector(".btn");
+let btn = document.querySelector(".add-btn");
+let del_btn=document.querySelector(".delete-btn");
+let clear_btn=document.querySelector(".clear-all")
 
 console.log(btn);
 
 
 
 btn.addEventListener('click',(e)=>{
+    const data = localStorage.getItem(key.value);
     e.preventDefault();
-    localStorage.setItem(`${key.value}`,`${value.value}`);
-    div1.innerHTML+=`Value:${key.value}`;
-    div2.innerHTML+=`Key:${value.value},added to Local Storage.`;
+    if(key.value.trim()!='')
+    {
+    localStorage.setItem(`${key.value}`,`${description.value}`);
+    div1.innerHTML+=`<h1>Title: ${key.value}</h1>`;
+    div2.innerHTML+=`<p>${description.value}`
+    }else{
+        alert("Add a note first.")
+    }
+    
 })
 
-div1.innerHTML+="";
-div2.innerHTML+="";
+//removes the data if it exists.
+del_btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+  const data = localStorage.getItem(key.value);
+  if (data) {
+    localStorage.removeItem(key.value);
+    div1.innerHTML = "";
+    div2.innerHTML = "";
+    document.querySelectorAll('.display-1 h1, .display-2 p').forEach(elem => elem.remove());
+  } else {
+    alert("Note does not exist in local storage.");
+  }
+})
+
+clear_btn.addEventListener('click',(e)=>{
+    const data = localStorage.getItem(key.value);
+    if (data) {
+      localStorage.clear();
+      div1.innerHTML = "";
+      div2.innerHTML = "";
+      document.querySelectorAll('.display-1 h1, .display-2 p').forEach(elem => elem.remove());
+    } else {
+      alert("No items to clear.");
+    }
+
+})
+
+
